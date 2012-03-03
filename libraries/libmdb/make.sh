e1b88d93ac8812635785dcc3f87ea015f454e022
Hallvard Furuseth <hallvard@openldap.org>
2012-03-03 09:52:33 +0100

    Remove development file libmdb/make.sh

--- libraries/libmdb/make.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-warning_filter() {
-    egrep -v ': In function .*:$|: warning: (ISO C99 requires rest arguments to be used|format .*%p.* has type .*struct MDB_.*\*)'
-}
-
-exit `{
-    { XCFLAGS="-std=c99 -pedantic" make "$@" 2>&1; echo $? >&3; } |
-    warning_filter >&2
-} 3>&1`
