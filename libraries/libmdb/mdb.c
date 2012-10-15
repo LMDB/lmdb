092f9a5c66425535f9d538d8fefd0049ad65f452
Hallvard Furuseth <hallvard@openldap.org>
2012-10-15 17:03:46 +0200

    More for mdb_env_copy().
    Fix #define _GNU_SOURCE = 1 to match any command line -D_GNU_SOURCE.
    .gitignore += mdb_copy.

--- libraries/libmdb/mdb.c
+++ libraries/libmdb/mdb.c
@@ -32,7 +32,7 @@
  * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
  * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
-#define _GNU_SOURCE
+#define _GNU_SOURCE 1
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <sys/param.h>
