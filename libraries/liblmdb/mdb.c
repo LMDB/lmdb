742a078ea5785ff6667cec0c3fccb045dc25c633
David Wilson <dw@botanicus.net>
2013-10-28 06:58:04 -0700

    ITS#7734 Add MDB_NORDAHEAD to CHANGELESS.

--- libraries/liblmdb/mdb.c
+++ libraries/liblmdb/mdb.c
@@ -3929,7 +3929,7 @@ fail:
 	 *	environment and re-opening it with the new flags.
 	 */
 #define	CHANGEABLE	(MDB_NOSYNC|MDB_NOMETASYNC|MDB_MAPASYNC)
-#define	CHANGELESS	(MDB_FIXEDMAP|MDB_NOSUBDIR|MDB_RDONLY|MDB_WRITEMAP|MDB_NOTLS|MDB_NOLOCK)
+#define	CHANGELESS	(MDB_FIXEDMAP|MDB_NOSUBDIR|MDB_RDONLY|MDB_WRITEMAP|MDB_NOTLS|MDB_NOLOCK|MDB_NORDAHEAD)
 
 int
 mdb_env_open(MDB_env *env, const char *path, unsigned int flags, mdb_mode_t mode)
