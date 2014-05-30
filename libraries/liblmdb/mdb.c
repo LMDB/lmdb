LMDB/lmdb

commit 4c8f57615c5ca7b014c038e59c1045182e74f5ad

Fix commit "Raise safe max MDB_MAXKEYSIZE."
I.e. d69d2ce2307e9d0e2167843935c386b05a869c31, it left out some changes.

libraries/liblmdb/mdb.c
@@ -4507,8 +4507,6 @@ mdb_node_search(MDB_cursor *mc, MDB_val *key, int *exactp)
 	}
 #endif
 
-	assert(nkeys > 0);
-
 	low = IS_LEAF(mp) ? 0 : 1;
 	high = nkeys - 1;
 	cmp = mc->mc_dbx->md_cmp;
@@ -4571,7 +4569,7 @@ mdb_node_search(MDB_cursor *mc, MDB_val *key, int *exactp)
 			node = NODEPTR(mp, i);
 	}
 	if (exactp)
-		*exactp = (rc == 0);
+		*exactp = (rc == 0 && nkeys > 0);
 	/* store the key index */
 	mc->mc_ki[mc->mc_top] = i;
 	if (i >= nkeys)
@@ -5968,7 +5966,8 @@ prep_subDB:
 			rdata = &xdata;
 			flags |= F_DUPDATA;
 			do_sub = 1;
-			mdb_node_del(mc->mc_pg[mc->mc_top], mc->mc_ki[mc->mc_top], 0);
+			if (!insert)
+				mdb_node_del(mc->mc_pg[mc->mc_top], mc->mc_ki[mc->mc_top], 0);
 			goto new_sub;
 		}
 current:

commit ac158c7e1e1a86b13eb7c0d2b14169c0e43e6198

ITS#7834 Fix MDB_MULTIPLE writing multiple keys.
Update the state before looping, so the key is not duplicated.
Broke in 4c8f57615c5ca7b014c038e59c1045182e74f5ad (mostly).

libraries/liblmdb/mdb.c
@@ -6138,7 +6138,7 @@ current:
 					data->mv_data = METADATA(omp);
 				else
 					memcpy(METADATA(omp), data->mv_data, data->mv_size);
-				goto done;
+				return MDB_SUCCESS;
 			  }
 			}
 			if ((rc2 = mdb_ovpage_free(mc, omp)) != MDB_SUCCESS)
@@ -6154,7 +6154,7 @@ current:
 				memcpy(olddata.mv_data, data->mv_data, data->mv_size);
 			else
 				memcpy(NODEKEY(leaf), key->mv_data, key->mv_size);
-			goto done;
+			return MDB_SUCCESS;
 		}
 		mdb_node_del(mc, 0);
 	}
@@ -6252,6 +6252,12 @@ put_sub:
 		/* Increment count unless we just replaced an existing item. */
 		if (insert_data)
 			mc->mc_db->md_entries++;
+		if (!rc && insert_key) {
+			/* If we succeeded and the key didn't exist before,
+			 * make sure the cursor is marked valid.
+			 */
+			mc->mc_flags |= C_INITIALIZED;
+		}
 		if (flags & MDB_MULTIPLE) {
 			if (!rc) {
 next_mult:
@@ -6260,17 +6266,12 @@ next_mult:
 				data[1].mv_size = mcount;
 				if (mcount < dcount) {
 					data[0].mv_data = (char *)data[0].mv_data + data[0].mv_size;
+					insert_key = insert_data = 0;
 					goto more;
 				}
 			}
 		}
 	}
-done:
-	/* If we succeeded and the key didn't exist before, make sure
-	 * the cursor is marked valid.
-	 */
-	if (!rc && insert_key)
-		mc->mc_flags |= C_INITIALIZED;
 	return rc;
 }
 