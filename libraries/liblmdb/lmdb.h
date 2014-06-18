59d30a49c3ad403235ba30f65e074eddf1c125f0
Howard Chu <hyc@symas.com>
2014-06-18 14:25:46 -0700

    Bump to 0.9.13

--- libraries/liblmdb/lmdb.h
+++ libraries/liblmdb/lmdb.h
@@ -184,7 +184,7 @@ typedef int mdb_filehandle_t;
 /** Library minor version */
 #define MDB_VERSION_MINOR	9
 /** Library patch version */
-#define MDB_VERSION_PATCH	12
+#define MDB_VERSION_PATCH	13
 
 /** Combine args a,b,c into a single integer for easy version comparisons */
 #define MDB_VERINT(a,b,c)	(((a) << 24) | ((b) << 16) | (c))
