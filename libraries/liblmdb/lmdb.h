ad8488cfac644d7a289e428ab3c403c859d844cb
Howard Chu <hyc@openldap.org>
2016-02-05 21:22:15 +0000

    Release 0.9.18

--- libraries/liblmdb/lmdb.h
+++ libraries/liblmdb/lmdb.h
@@ -204,7 +204,7 @@ typedef int mdb_filehandle_t;
 	MDB_VERINT(MDB_VERSION_MAJOR,MDB_VERSION_MINOR,MDB_VERSION_PATCH)
 
 /** The release date of this library version */
-#define MDB_VERSION_DATE	"December 19, 2015"
+#define MDB_VERSION_DATE	"February 5, 2016"
 
 /** A stringifier for the version info */
 #define MDB_VERSTR(a,b,c,d)	"LMDB " #a "." #b "." #c ": (" d ")"
