dda2ac0a1b1d7e9399b85817bc98f277287f6f23
Howard Chu <hyc@openldap.org>
2016-04-26 12:52:21 +0100

    ITS#8412 fix NEXT_DUP after cursor_del

--- libraries/liblmdb/mdb.c
+++ libraries/liblmdb/mdb.c
@@ -5641,7 +5641,8 @@ mdb_cursor_next(MDB_cursor *mc, MDB_val *key, MDB_val *data, MDB_cursor_op op)
 	MDB_node	*leaf;
 	int rc;
 
-	if (mc->mc_flags & C_EOF) {
+	if ((mc->mc_flags & C_EOF) ||
+		((mc->mc_flags & C_DEL) && op == MDB_NEXT_DUP)) {
 		return MDB_NOTFOUND;
 	}
 	if (!(mc->mc_flags & C_INITIALIZED))
