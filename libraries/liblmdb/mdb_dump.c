0a2622317f189c7062d03d050be6766586a548b2
Quanah Gibson-Mount <quanah@openldap.org>
2018-03-22 15:20:57 +0000

    Happy New Year

--- libraries/liblmdb/mdb_dump.c
+++ libraries/liblmdb/mdb_dump.c
@@ -1,6 +1,6 @@
 /* mdb_dump.c - memory-mapped database dump tool */
 /*
- * Copyright 2011-2017 Howard Chu, Symas Corp.
+ * Copyright 2011-2018 Howard Chu, Symas Corp.
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
