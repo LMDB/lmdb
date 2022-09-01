LMDB/lmdb

commit d85fe32dab55b88cec25fc73dadc6d38df807cce

ITS#9372 Default to using POSIX semaphores on MacOS
Unless robust is specified (then use SysV)

libraries/liblmdb/mdb.c
@@ -164,6 +164,8 @@ typedef SSIZE_T	ssize_t;
 #if defined(__FreeBSD__) && defined(__FreeBSD_version) && __FreeBSD_version >= 1100110
 # define MDB_USE_POSIX_MUTEX	1
 # define MDB_USE_ROBUST	1
+#elif defined(__APPLE__) && !defined(MDB_USE_ROBUST)
+# define MDB_USE_POSIX_SEM	1
 #elif defined(__APPLE__) || defined (BSD) || defined(__FreeBSD_kernel__)
 # if !(defined(MDB_USE_POSIX_MUTEX) || defined(MDB_USE_POSIX_SEM))
 # define MDB_USE_SYSV_SEM	1

commit ad3f8367b0ef913dbff074ddd0fdcbcaa86fdbb0

ITS#9910 fix undefined MDB_FDATASYNC on MacOSX
Broken by d85fe32 ITS#9372

libraries/liblmdb/mdb.c
@@ -164,9 +164,10 @@ typedef SSIZE_T	ssize_t;
 #if defined(__FreeBSD__) && defined(__FreeBSD_version) && __FreeBSD_version >= 1100110
 # define MDB_USE_POSIX_MUTEX	1
 # define MDB_USE_ROBUST	1
-#elif defined(__APPLE__) && !defined(MDB_USE_ROBUST)
-# define MDB_USE_POSIX_SEM	1
 #elif defined(__APPLE__) || defined (BSD) || defined(__FreeBSD_kernel__)
+# if defined(__APPLE__) && !defined(MDB_USE_ROBUST)
+# define MSB_USE_POSIX_SEM	1
+# endif
 # if !(defined(MDB_USE_POSIX_MUTEX) || defined(MDB_USE_POSIX_SEM))
 # define MDB_USE_SYSV_SEM	1
 # endif